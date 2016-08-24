58ecffbe2054059b1c6c41f5d76c42a6685c07b9
Adriano <carmezim.filho@gmail.com>
2016-08-24 02:52:12 +0200

    Adding all folders and basic files

---
+++ js/app.js
@@ -0,0 +1,70 @@
+import React		 from 'react';
+import ReactDOM  from 'react-dom';
+import { Router, Route, IndexRoute } from 'react-router';
+import CSS			 from '../css/app.less';
+import AppHeader from 'appRoot/views/appHeader';
+import Login     from 'appRoot/views/login';
+import PostList  from 'appRoot/views/posts/list';
+import PostView  from 'appRoot/views/posts/view';
+import PostEdit  from 'appRoot/views/posts/edit';
+import UserList  from 'appRoot/views/users/view';
+import UserView  from 'appRoot/views/users/view';
+import UserEdit  from 'appRoot/views/users/view';
+
+let AppLayout = React.createClass({
+	render: function () {
+		return (
+				<div className="app-container">
+					<AppHeader />
+					<main>
+						{React.cloneElement(this.props.children, this.props)}
+					</main>
+				</div>
+	  );
+	}
+});
+
+let routes = {
+	<Route path"/" component={ AppLayout }>
+		<IndexRoute component= { PostList } />
+			<Route
+				path="posts/:pageNum/?"
+				component={ PostList }
+				ignoreScrollBehavior
+			/>
+			<Route
+				path="/posts/create"
+				component= { PostEdit }
+			/>
+			<Route
+				path="/posts/:postId/edit"
+				component= { PostView  }
+			/>
+			<Route
+				path="posts/:postId"
+				component= { PostView }
+			/>	
+			<Route
+				path="/users"
+				component= { UserList }
+			/>	
+			<Route
+				path="/users/create"
+				component= { UserEdit }
+			/>
+			<Route
+				path="/users/:userId"
+				component= { UserView }
+			/>	
+			<Route
+				path="users/:userId/edit"
+				component= { UserEdit }
+			/>
+			<Route path="*" component={ PostList } />
+	</Route>		
+};
+
+ReactDOM.render(<Router>{routes}</Router>, document.getElementById('app'));
+
+
+					
