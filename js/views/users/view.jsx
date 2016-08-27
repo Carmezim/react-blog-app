c576f041ae55302db1b598f26fe0f826775fbf8b
Adriano <carmezim.filho@gmail.com>
2016-08-27 22:42:34 +0200

    users view

--- js/views/users/view.jsx
+++ js/views/users/view.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React     from 'react';
+import UserView  from 'appRoot/components/users/view';
 
 export default React.createClass({
 	render: function () {
-		return ((<div className="user-view">user view</div>);
-  }
+		return (
+			<div className="user-view">
+				<UserView userId={this.props.params.userId} />
+			</div>
+		);
+	}
 });
