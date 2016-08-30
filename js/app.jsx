e0ff3b485d64cfbda09e8e74d66a35c019c4dff7
Adriano <carmezim.filho@gmail.com>
2016-08-30 23:41:41 +0200

    adding changes and updating js

--- js/app.jsx
+++ js/app.jsx
@@ -1,70 +1,75 @@
-import React		 from 'react';
-import ReactDOM  from 'react-dom';
+"use strict";
+
+import React     from 'react';
+import ReactDom  from 'react-dom';
 import { Router, Route, IndexRoute } from 'react-router';
-import CSS			 from '../css/app.less';
+import CSS       from '../css/app.less';
 import AppHeader from 'appRoot/views/appHeader';
 import Login     from 'appRoot/views/login';
 import PostList  from 'appRoot/views/posts/list';
 import PostView  from 'appRoot/views/posts/view';
 import PostEdit  from 'appRoot/views/posts/edit';
-import UserList  from 'appRoot/views/users/view';
+import UserList  from 'appRoot/views/users/list';
 import UserView  from 'appRoot/views/users/view';
-import UserEdit  from 'appRoot/views/users/view';
+import UserEdit  from 'appRoot/views/users/edit';
 
+// Components must be uppercase - regular DOM is lowercase
+// https://facebook.github.io/react/docs/jsx-in-depth.html#html-tags-vs.-react-components
 let AppLayout = React.createClass({
-	render: function () {
-		return (
-				<div className="app-container">
-					<AppHeader />
-					<main>
-						{React.cloneElement(this.props.children, this.props)}
-					</main>
-				</div>
-	  );
-	}
+  render: function () {
+    return (
+      <div className="app-container">
+        <AppHeader />
+        <main>
+          {React.cloneElement(this.props.children, this.props)}
+        </main>
+      </div>
+    );
+  }
 });
 
-let routes = {
-	<Route path"/" component={ AppLayout }>
-		<IndexRoute component= { PostList } />
-			<Route
-				path="posts/:pageNum/?"
-				component={ PostList }
-				ignoreScrollBehavior
-			/>
-			<Route
-				path="/posts/create"
-				component= { PostEdit }
-			/>
-			<Route
-				path="/posts/:postId/edit"
-				component= { PostView  }
-			/>
-			<Route
-				path="posts/:postId"
-				component= { PostView }
-			/>	
-			<Route
-				path="/users"
-				component= { UserList }
-			/>	
-			<Route
-				path="/users/create"
-				component= { UserEdit }
-			/>
-			<Route
-				path="/users/:userId"
-				component= { UserView }
-			/>	
-			<Route
-				path="users/:userId/edit"
-				component= { UserEdit }
-			/>
-			<Route path="*" component={ PostList } />
-	</Route>		
-};
+let routes = (
+  <Route path="/" component={ AppLayout }>
+    <IndexRoute component={ PostList } />
+    <Route
+      path="posts/:pageNum/?"
+      component={ PostList }
+      ignoreScrollBehavior
+    />
+    <Route
+      path="/posts/create"
+      component={ PostEdit }
+    />
+    <Route
+      path="/posts/:postId/edit"
+      component={ PostEdit }
+    />
+    <Route
+      path="posts/:postId"
+      component={ PostView }
+    />
+    <Route
+      path="/users"
+      component={ UserList }
+    />
+    <Route
+      path="/users/create"
+      component={ UserEdit }
+    />
+    <Route
+      path="/users/:userId"
+      component={ UserView }
+    />
+    <Route
+      path="/users/:userId/edit"
+      component={ UserEdit }
+    />
+    <Route
+      path="/login"
+      component={ Login }
+    />
+    <Route path="*" component={ PostList } />
+  </Route>
+);
 
-ReactDOM.render(<Router>{routes}</Router>, document.getElementById('app'));
-
-
-					
+ReactDom.render(<Router>{routes}</Router>, document.getElementById('app'));
--- js/views/appHeader.jsx
+++ js/views/appHeader.jsx
@@ -1,16 +1,40 @@
-import React 		from 'react';
-import { Link } from 'react-router';
+"use strict"
+
+import React 							from 'react';
+import Reflux 						from 'reflux';
+import Actions 						from 'appRoot/actions';
+import SessionStore				from 'appRoot/stores/sessionContext';
+import { Link, History }  from 'react-router';
 
 export default React.createClass({
+	mixins: [
+		Reflux.connect(SesssionStore, 'session'),
+		History
+	],
+	logOut: function () {
+		Actions.logOut();
+		this.history.pushState('', '/');
+	},
 	render: function () {
 		return (
-				<header className="app-header">
-					<Link to="/"><h1>ReAction</h1></Link>
-					<section className="account-ctrl">
-						<Link to="/users/create">Join</Link>
-						<Link to"/login">Log In</Link>
-					</section>
-				</header>
+			<header className="app-header">
+				<Link to="/"><h1>ReAction</h1></Link>
+				<section className="account-ctrl">
+					{
+						this.state.session.loggedIn ?
+							(<Link to="/posts/create">
+								Hello {this.state.session.username}, write something!
+							 </Link>) :
+							 <Link to="/users/create">Join</Link>
+					}
+					{
+						this.state.session.loggedIn ?
+							<a onClick={this.logOut}>Log Out</a> :
+							<Link to="/login">Log in</Link>
+					}
+				</section>
+			</header>
 		);
   }
 });
+
