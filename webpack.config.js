55eb3a78107979c6c498147846d7d8e05e2a489c
Adriano <carmezim.filho@gmail.com>
2016-08-30 20:46:49 +0200

    adding simplified and fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,54 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin")
-var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-	template: __dirname + "/index.html",
-	filename: "index.html",
-	inject: "body"
-});
+var path    = require('path')
+  ,   webpack = require('webpack')
+  ;
 
 module.exports = {
-	entry: "./src/main.js",
-	output: {
-				path: __dirname + "/dist",
-				filename: "bundle.js",
-				publicPath: "assets"
-	},
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
+    'webpack/hot/only-dev-server',
+    'babel-polyfill',
+    './js/app' // Your appʼs entry point
+  ],
+  output: {
+    filename: "js/bundle.js"
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
 
-	module: {
-		loaders: [
-			{
-				test: /\.js$/, exclude: /node_modules/, 
-				loader: ['babel-loader'], 
-				include: __dirname,
-				query: {
-					presets: ["react", "es2015", "stage-0", "react-hmre"]
-				},
-			},	
-			{
-				test: /\.json$/, 
-				loader: "json",		
-			},
-			{
-				test: /\.css$/,	
-				loader: "style-loader!css-loader!postcss-loader"
-			}
-		]
-	},
-
-	resolve: {
-		extensios: ['', '.js', '.jsx']
-	},
-
-	postcss: ['autoprefixer'],
-
-	plugins: [HTMLWebpackPluginConfig],	
-
-	devtool: 'source-map',
-	devServer: { //config for webpack dev server
-		colors: true,
-		historyApiFallback: true,
-		inline: true,
-		hot: true,
-	}				
-};	
-
-
-
+    // example of polyfilling with webpack
+    // OR just include the babel runtime option below and get Promises, Generators, Map, and much more!
+    // You can even get forward looking proposed features for ES7 and beyond with the
+    // stage query parameter below https://babeljs.io/docs/usage/experimental/ welcome
+    // to the future of JavaScript! :)
+    //
+    //new webpack.ProvidePlugin({
+    //	'arrayutils': 'imports?this=>global!exports?global.arrayutils!arrayutils'
+    //})
+  ],
+  resolve: {
+    // require files in app without specifying extensions
+    extensions: ['', '.js', '.json', '.jsx', '.less'],
+    alias: {
+      // pretty useful to have a starting point in nested modules
+      'appRoot': path.join(__dirname, 'js'),
+      'vendor': 'appRoot/vendor'
+    }
+  },
+  module: {
+    loaders: [
+      { test: /\.less$/,      loader: 'style-loader!css-loader!autoprefixer?browsers=last 2 version!less-loader' },
+      { test: /\.css$/,       loader: 'style-loader!css-loader' },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'}, // inline base64 URLs for <=8k images, direct URLs for the rest
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.join(__dirname, 'js'), // files to apply this loader to
+          path.join(__dirname, 'node_modules/reflux-core')
+        ],
+        // http://jamesknelson.com/using-es6-in-the-browser-with-babel-6-and-webpack/
+        loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015', 'reflux-wrap-loader'] // loaders process from right to left
+      }
+    ]
+  }
+};
