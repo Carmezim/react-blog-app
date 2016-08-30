28036a23144df11ff9747664ece7328a75c346da
Adriano <carmezim.filho@gmail.com>
2016-08-30 23:44:52 +0200

    fixing indent

--- js/views/appHeader.jsx
+++ js/views/appHeader.jsx
@@ -7,34 +7,34 @@ import SessionStore				from 'appRoot/stores/sessionContext';
 import { Link, History }  from 'react-router';
 
 export default React.createClass({
-	mixins: [
-		Reflux.connect(SesssionStore, 'session'),
-		History
-	],
-	logOut: function () {
-		Actions.logOut();
-		this.history.pushState('', '/');
-	},
-	render: function () {
-		return (
-			<header className="app-header">
-				<Link to="/"><h1>ReAction</h1></Link>
-				<section className="account-ctrl">
-					{
-						this.state.session.loggedIn ?
-							(<Link to="/posts/create">
-								Hello {this.state.session.username}, write something!
-							 </Link>) :
-							 <Link to="/users/create">Join</Link>
-					}
-					{
-						this.state.session.loggedIn ?
-							<a onClick={this.logOut}>Log Out</a> :
-							<Link to="/login">Log in</Link>
-					}
-				</section>
-			</header>
-		);
+  mixins: [
+    Reflux.connect(SesssionStore, 'session'),
+    History
+  ],
+  logOut: function () {
+    Actions.logOut();
+    this.history.pushState('', '/');
+  },
+  render: function () {
+    return (
+      <header className="app-header">
+        <Link to="/"><h1>ReAction</h1></Link>
+        <section className="account-ctrl">
+          {
+            this.state.session.loggedIn ?
+              (<Link to="/posts/create">
+                Hello {this.state.session.username}, write something!
+               </Link>) :
+               <Link to="/users/create">Join</Link>
+          }
+          {
+            this.state.session.loggedIn ?
+              <a onClick={this.logOut}>Log Out</a> :
+              <Link to="/login">Log in</Link>
+          }
+        </section>
+      </header>
+    );
   }
 });
 
